7e749f6ae8d8971c9a200707d033e4d9dc4f1a2d
Tyuiu.NezhdanovDA@outlook.com <nezhdanov322@mail.ru>
2023-10-19 23:06:43 +0500

    Add project files.

---
+++ Tyuiu.NezhdanovDA.Sprint2.Task0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NezhdanovDA.Sprint2.Task0.Lib;
+namespace Tyuiu.NezhdanovDA.Sprint2.Task0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValicdCompairOperations()
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            int y = 16;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, true, true, true, true, true };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NezhdanovDA.Sprint2.Task0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NezhdanovDA.Sprint2.Task0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NezhdanovDA.Sprint2.Task0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80c382c5-3ea2-4c19-9831-5572b9fc6466")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
